20da5645891d534f5469a18ade97b4c656c7f9ef
Chris Palka <cpalka87@gmail.com>
2023-09-27 20:46:08 -0600

    Add headers

--- netlify/functions/sec.js
+++ netlify/functions/sec.js
@@ -16,6 +16,12 @@ exports.handler = async function () {
   const filings = await queryApi.getFilings(query);
   return {
     statusCode: 200,
+    headers: {
+      /* Required for CORS support to work */
+      'Access-Control-Allow-Origin': '*',
+      /* Required for cookies, authorization headers with HTTPS */
+      'Access-Control-Allow-Credentials': true,
+    },
     body: JSON.stringify({
       message: filings,
     }),
